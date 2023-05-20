104d9d5538038be49f03773586127583a97a0d38
TruongAnim <truonganim.dev@gmail.com>
2023-05-20 14:46:38 +0700

    add first problem

---
+++ codeforces/problemset/problem/4/A/main.py
@@ -0,0 +1,11 @@
+def main():
+    a = int(input())
+    if a > 2 and a % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+# Check if this file is being run directly (not imported)
+if __name__ == "__main__":
+    main()
