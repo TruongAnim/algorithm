65f50747694b4e5a33a15117cd48d12f2bcff241
TruongAnim <truonganim.dev@gmail.com>
2023-05-23 15:57:50 +0700

    add problem Team

---
+++ codeforces/problemset/problem/231/A/main.py
@@ -0,0 +1,14 @@
+def main():
+    import sys
+    sys.stdin = open('input.txt', 'r')
+    n = int(input())
+    sum = 0
+    for i in range(n):
+        a, b, c = map(int, input().split())
+        if a + b + c > 1:
+            sum += 1
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
