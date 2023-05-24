5248e5e0f99173dcd5a04af6c8e6659188266460
TruongAnim <truonganim.dev@gmail.com>
2023-05-24 10:45:49 +0700

    add problem Petya and Strings

---
+++ codeforces/problemset/problem/112/A/main.py
@@ -0,0 +1,15 @@
+def main():
+    import sys
+    sys.stdin = open('input.txt', 'r')
+    str1 = input().lower()
+    str2 = input().lower()
+    if str1 < str2:
+        print(-1)
+    elif str1 > str2:
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    main()
