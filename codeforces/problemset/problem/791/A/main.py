435114b6a350947046111d9a8a3feef7b0631910
TruongAnim <truonganim.dev@gmail.com>
2023-05-30 13:38:37 +0700

    add Bear and Big Brother problem

---
+++ codeforces/problemset/problem/791/A/main.py
@@ -0,0 +1,12 @@
+def main():
+    limak, bob = map(int, input().split(' '))
+    year = 0
+    while limak <= bob:
+        year += 1
+        limak *= 3
+        bob *= 2
+    print(year)
+
+
+if __name__ == '__main__':
+    main()
