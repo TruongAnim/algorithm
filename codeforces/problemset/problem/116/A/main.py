7af980f34fdf5305c6d1a24f94ac48a93e5e1240
TruongAnim <truonganim.dev@gmail.com>
2023-06-03 14:25:10 +0700

    add Tram problem

---
+++ codeforces/problemset/problem/116/A/main.py
@@ -0,0 +1,17 @@
+def main():
+    import sys
+    sys.stdin = open('input.txt', 'r')
+    n = int(input())
+    capacity = 0
+    max = 0
+    for i in range(n):
+        exit, enter = map(int, input().split(' '))
+        capacity -= exit
+        capacity += enter
+        if max < capacity:
+            max = capacity
+    print(max)
+
+
+if __name__ == '__main__':
+    main()
