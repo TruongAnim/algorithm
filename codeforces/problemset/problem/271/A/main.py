1ece9feec20da46aef1edd8c170ea898c17ff19a
TruongAnim <truonganim.dev@gmail.com>
2023-06-03 15:30:28 +0700

    add Beautiful Year problem

---
+++ codeforces/problemset/problem/271/A/main.py
@@ -0,0 +1,14 @@
+def main():
+    year = int(input())
+    for i in range(year + 1, 10000):
+        if check_distinct(str(i)):
+            print(i)
+            return
+
+
+def check_distinct(number):
+    return len(number) == len(set(number))
+
+
+if __name__ == '__main__':
+    main()
