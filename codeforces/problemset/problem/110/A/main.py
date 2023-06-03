1cdc95f0ffc1ea22c0d937d458e6557ce7d4b181
TruongAnim <truonganim.dev@gmail.com>
2023-06-03 14:02:11 +0700

    add Nearly Lucky Number problem

---
+++ codeforces/problemset/problem/110/A/main.py
@@ -0,0 +1,14 @@
+def main():
+    number = input()
+    l_number = 0
+    for i in number:
+        if i == '4' or i == '7':
+            l_number+=1
+    if l_number == 4 or l_number == 7:
+        print('YES')
+    else:
+        print('NO')
+
+
+if __name__=='__main__':
+    main()
