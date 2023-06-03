6b70ed200678e8cd74861c261c41590cacde3a1b
TruongAnim <truonganim.dev@gmail.com>
2023-06-03 14:31:53 +0700

    add Translation problem

---
+++ codeforces/problemset/problem/41/A/main.py
@@ -0,0 +1,11 @@
+def main():
+    word1 = input()
+    word2 = input()
+    if word1 == word2[::-1]:
+        print('YES')
+    else:
+        print('NO')
+
+
+if __name__ == '__main__':
+    main()
