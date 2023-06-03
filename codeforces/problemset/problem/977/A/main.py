c538a4ae3cc0b007b4cdd27693d3a5a36526682c
TruongAnim <truonganim.dev@gmail.com>
2023-06-03 13:55:48 +0700

    add Wrong Subtraction problem

---
+++ codeforces/problemset/problem/977/A/main.py
@@ -0,0 +1,12 @@
+def main():
+    number, k = map(int, input().split(' '))
+    for i in range(k):
+        if number % 10 == 0:
+            number = number // 10
+        else:
+            number -= 1
+    print(number)
+
+
+if __name__ == '__main__':
+    main()
