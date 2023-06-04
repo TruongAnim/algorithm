1ddefacaf8b2af8065920ca9db8abdb3b905218d
TruongAnim <truonganim.dev@gmail.com>
2023-06-04 15:58:38 +0700

    add Magnets problem

---
+++ codeforces/problemset/problem/344/A/main.py
@@ -0,0 +1,14 @@
+def main():
+    n = int(input())
+    sum = 1
+    pre = input()
+    for i in range(n - 1):
+        magnet = input()
+        if pre[-1] == magnet[0]:
+            sum += 1
+        pre = magnet
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
