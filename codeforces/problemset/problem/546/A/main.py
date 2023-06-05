718135f666540ecb872986d0c8d1f0f3057b5b16
TruongAnim <truonganim.dev@gmail.com>
2023-06-06 06:33:45 +0700

    add Soldier and Bananas problem

---
+++ codeforces/problemset/problem/546/A/main.py
@@ -0,0 +1,13 @@
+def main():
+    k, n, w = map(int, input().split(' '))
+    sum = 0
+    for i in range(w):
+        sum += k * (i + 1)
+    if sum <= n:
+        print(0)
+    else:
+        print(sum - n)
+
+
+if __name__ == '__main__':
+    main()
