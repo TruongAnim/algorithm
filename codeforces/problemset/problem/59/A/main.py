38bd9915bf7e235d6f1ef2f0c363511a4ae59b36
TruongAnim <truonganim.dev@gmail.com>
2023-06-06 06:47:55 +0700

    add Word problem

---
+++ codeforces/problemset/problem/59/A/main.py
@@ -0,0 +1,17 @@
+def main():
+    word = input()
+    upper = 0
+    lower = 0
+    for i in word:
+        if i.isupper():
+            upper += 1
+        else:
+            lower += 1
+    if lower >= upper:
+        print(word.lower())
+    else:
+        print(word.upper())
+
+
+if __name__ == '__main__':
+    main()
