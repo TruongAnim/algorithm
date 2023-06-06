b5c2f652f31d8abd73cdbeeb8705b2e1e6eff117
TruongAnim <truonganim.dev@gmail.com>
2023-06-07 06:04:29 +0700

    add Cipher Shifer problem

---
+++ codeforces/contest/1840/problem/A/main.py
@@ -0,0 +1,20 @@
+def main():
+    import sys
+    sys.stdin = open('input.txt', 'r')
+    testcase = int(input())
+    for _ in range(testcase):
+        n = int(input())
+        encrypt = input()
+        result = ''
+        pre = ''
+        for i in encrypt:
+            if pre == '':
+                pre = i
+                result += i
+            elif pre == i:
+                pre = ''
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
