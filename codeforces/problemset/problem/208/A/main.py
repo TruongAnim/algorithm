5d13469d4f00d7a0505552c657494e86454d2897
TruongAnim <truonganim.dev@gmail.com>
2023-06-11 16:38:01 +0700

    add Dubstep problem

---
+++ codeforces/problemset/problem/208/A/main.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/208/A
+
+def main():
+    s = input().split('WUB')
+    for i in s:
+        print(i, end=' ')
+
+
+if __name__ == '__main__':
+    main()
