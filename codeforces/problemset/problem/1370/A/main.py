cfe4d8546fba2826e61a7afe3372e436430fd436
TruongAnim <truonganim.dev@gmail.com>
2023-06-28 20:02:06 +0700

    add Maximum GCD problem

---
+++ codeforces/problemset/problem/1370/A/main.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/problemset/problem/1370/A
+
+def main():
+    test = int(input())
+    for _ in range(test):
+        n = int(input())
+        print(n // 2)
+
+
+if __name__ == '__main__':
+    main()
