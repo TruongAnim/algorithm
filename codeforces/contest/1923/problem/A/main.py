95742c2cd714eba2512de1995d39466af03b89c7
TruongAnim <truonganim.dev@gmail.com>
2024-02-26 19:44:37 +0700

    add Moving Chips problem

---
+++ codeforces/contest/1923/problem/A/main.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/contest/1923/problem/A
+
+def main():
+    test = int(input())
+    for _ in range(test):
+        s = input()
+        c = 0
+        for i in s:
+            if i == 'A':
+                c += 1
+        if c < 3:
+            print('B')
+        else:
+            print('A')
+
+
+if __name__ == '__main__':
+    main()
