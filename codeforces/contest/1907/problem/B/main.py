f6548b8a53f9cdadbf8cd13f604c7720bf164676
TruongAnim <truonganim.dev@gmail.com>
2024-02-27 19:38:03 +0700

    add YetnotherrokenKeoard problem

---
+++ codeforces/contest/1907/problem/B/main.py
@@ -0,0 +1,31 @@
+# https://codeforces.com/contest/1907/problem/B
+
+def main():
+    # import sys
+    # sys.stdin = open('input.txt', 'r')
+    test = int(input())
+    for t in range(test):
+        s = input()
+        temp = []
+        il = 0
+        ul = 0
+        for i in s[::-1]:
+            if i == 'B':
+                ul += 1
+            elif i == 'b':
+                il += 1
+            elif i.isupper():
+                if ul > 0:
+                    ul -= 1
+                else:
+                    temp.append(i)
+            elif i.islower():
+                if il > 0:
+                    il -= 1
+                else:
+                    temp.append(i)
+        print(''.join(temp[::-1]))
+
+
+if __name__ == '__main__':
+    main()
