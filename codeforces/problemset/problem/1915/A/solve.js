d58619e631ae47dc010e867adae1a35b5fa12f3c
TruongAnim <truonganim.dev@gmail.com>
2024-12-10 00:06:04 +0700

    add 1915 problem

--- codeforces/problemset/problem/1915/A/solve.js
+++ codeforces/problemset/problem/1915/A/solve.js
@@ -1,5 +1,5 @@
 "use strict"
-// https://codeforces.com/problemset/problem/1807/A
+// https://codeforces.com/problemset/problem/1915/A
 
 // Util
 const fs = require('fs');
@@ -27,10 +27,14 @@ function main() {
     const test = +readline()
     for (let t = 0; t < test; t++) {
         let l = readline().trim().split(' ').map(Number)
-        if (l[0] + l[1] == l[2]) {
-            print('+')
-        } else {
-            print('-')
+        if (l[0] == l[1]) {
+            print(l[2])
+        }
+        if (l[0] == l[2]) {
+            print(l[1])
+        }
+        if (l[1] == l[2]) {
+            print(l[0])
         }
     }
 }
