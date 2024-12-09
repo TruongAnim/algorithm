111445b2a4f9170723a279b0b27f2632e274743e
TruongAnim <truonganim.dev@gmail.com>
2024-12-09 23:58:45 +0700

    add 1807 problem

---
+++ codeforces/problemset/problem/1807/A/solve.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// Util
+const fs = require('fs');
+
+let input = []
+let readIndex = 0
+let readInput = () => {
+    fs.readFile('input.txt', 'utf8', (err, data) => {
+        input = data.split('\n');
+        main()
+    })
+}
+readInput()
+
+function readline() {
+    return input[readIndex++]
+}
+
+function print(msg) {
+    console.log(msg)
+}
+
+// solving start
+function main() {
+    const test = +readline()
+    for (let t = 0; t < test; t++) {
+        let l = readline().trim().split(' ').map(Number)
+        if (l[0] + l[1] == l[2]) {
+            print('+')
+        } else {
+            print('-')
+        }
+    }
+}
